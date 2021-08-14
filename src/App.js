11cde8f847841c882a26d96abd71c7484019f711
luisbryandr <luisbdominguezr@gmail.com>
2021-08-14 18:33:29 -0400

    working through setting up the app component and use effect in that file

--- src/App.js
+++ src/App.js
@@ -1,17 +1,34 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
+import axios from 'axios';
+import Character from './components/Character'
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
+const [characters,setCharacters] = useState([])//setting the state initially to an empty array that will be filled with the effect of fetching the characters from the API
+const [error, setError] =  useState(null);
 
+
+useEffect(()=>{
+  axios.get('https://swapi.dev/api/people')
+  .then(res => {
+    setCharacters(res.data)
+  })
+  .catch(err =>{
+    console.log(err);
+    return 'error Chewie & Solo is working on fixing it'
+  })
+})
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
   return (
     <div className="App">
-      <h1 className="Header">Characters</h1>
+      {error && <h1>{error}</h1>}
+      <h1 className="Header">Star Wars Characters</h1>
+      
     </div>
   );
 }
